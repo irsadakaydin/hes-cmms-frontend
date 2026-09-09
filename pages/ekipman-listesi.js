d89cb53f6d425fe9f70a1471e0392c13517013cd
irsadakaydin <irsadakaydin@hotmail.com>
2026-09-09 20:18:53 +0300

    Add files via upload

--- pages/ekipman-listesi.js
+++ pages/ekipman-listesi.js
@@ -227,7 +227,6 @@ export default function EkipmanListesiSayfasi() {
                 santralId={seciliSantralId}
                 santralAdi={(santraller || []).find((s) => s.santral_id === seciliSantralId)?.ad}
                 mod="ekipman"
-                gorunum="liste"
                 onSecim={klasordekiEkipmanlariGetir}
               />
             </div>
